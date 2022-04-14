4c4df5e7b9ae6f6493cd0ccb444e440349c37e8a
Moura <59957939+big0d@users.noreply.github.com>
2022-04-13 21:10:55 -0300

    Delete python directory

--- python/soma.py
+++
@@ -1,5 +0,0 @@
-numero1 = int(input('Digite um valor: '))
-numero2 = int(input('Digite mais um valor: '))
-soma = numero1 + numero2
-
-print('O resultado da soma de {} com {} é igual a {}'.format(numero1,numero2,soma))
--- python/welcome.py
+++
@@ -1,3 +0,0 @@
-nome = input('Qual é o seu nome?')
-print('Seja bem-vindo, {}!'.format(nome))
-print('É um enorme prazer te conhecer.')
