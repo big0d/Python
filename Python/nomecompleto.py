34d6cdaed55d53251ce879a7132df4f9e7cd054d
Moura <59957939+big0d@users.noreply.github.com>
2022-04-14 00:59:04 -0300

    Add files via upload

---
+++ Python/nomecompleto.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome em maiusculas {}'.format(nome.upper()))
+print('Seu nome em minusculas {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome)-nome.count(' ')))
+print('seu primeiro nome tem {} letras'.format(nome.find(' ')))
