9d7bf6032cf29eccb373de2d15fc52d7c4a2edbd
Moura <59957939+big0d@users.noreply.github.com>
2022-04-18 12:41:08 -0300

    Update maiormenorvalor.py

--- Python/maiormenorvalor.py
+++ Python/maiormenorvalor.py
@@ -4,12 +4,12 @@ v3 = float(input('Insira o terceiro valor: '))
 menor = v1
 if v2 < v1 and v2 < v3:
     menor = v2
-if v3 < v1 and v3 < v2:
+elif v3 < v1 and v3 < v2:
     menor = v3
 maior = v1
-if v2 > v1 and v2 > v3:
+elif v2 > v1 and v2 > v3:
     maior = v2
-if v3 > v1 and v3 > v2:
+elif v3 > v1 and v3 > v2:
     maior = v3
 print('O menor valor é {}'.format(menor))
 print('O maior valor é {}'.format(maior))
